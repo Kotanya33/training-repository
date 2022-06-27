b9e07e7b8461b77186586a89ca2af9db71cb2455
Tatiana <052025199@mail.ru>
2022-06-27 19:56:48 +0400

    Task 4

---
+++ Семинары/HomeWork_1/Task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿int n;
+int count;
+
+Console.Write("Введите число ");
+n = Convert.ToInt32(Console.ReadLine());
+
+count = 2;
+while(count <= n)
+{
+Console.Write(count + " ");
+count = count + 2;
+}
