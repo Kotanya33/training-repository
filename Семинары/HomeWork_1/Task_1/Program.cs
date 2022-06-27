a2528f0da61ed58feab8b53e6235c598de157f9c
Tatiana <052025199@mail.ru>
2022-06-27 19:13:10 +0400

    Task 1

---
+++ Семинары/HomeWork_1/Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿int num1;
+int num2;
+
+Console.Write("Введите первое число ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine("max = " + num1);
+}
+else
+{
+    Console.WriteLine("max = " + num2);
+}
