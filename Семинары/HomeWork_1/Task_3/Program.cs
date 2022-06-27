4f5367f439278b3be80d424c6e30d04839028c47
Tatiana <052025199@mail.ru>
2022-06-27 19:44:09 +0400

    Task 3

---
+++ Семинары/HomeWork_1/Task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿int num;
+int del;
+
+Console.Write("Введите число ");
+num = Convert.ToInt32(Console.ReadLine());
+
+del = num / 2;
+
+if(del * 2 == num) 
+{
+Console.WriteLine("четное");
+}
+else
+{
+Console.WriteLine("нечетное");
+}
