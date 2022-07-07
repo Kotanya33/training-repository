90e9cc38a25e27c8fac80028755a81ec78260078
Tatyana <052025199@mail.ru>
2022-07-07 23:12:42 +0300

    HW_5

---
+++ Семинары/HomeWork_5/Program.cs
@@ -0,0 +1,89 @@
+﻿
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] newArray = new int[size];
+    for(int i = 0; i < size; i++)
+        newArray[i] = new Random().Next(min, max + 1);
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+double[] CreateArray(int size)
+{
+    double[] newArray = new double[size];
+    Console.WriteLine("Создайте массив");
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент: ");
+        newArray[i] = Convert.ToDouble(Console.ReadLine());
+    }
+    return newArray;
+}
+
+int EvenNumbers(int[] array)
+{
+    int result = 0;
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] % 2 == 0) result++;
+    return result;
+}
+
+int SumOddPositions(int[] array)
+{
+    int sum = 0;
+    for(int i = 1; i < array.Length; i = i + 2)
+        sum += array[i];
+    return sum;
+}
+
+double DifferenceMinMax(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    for(int i = 1; i < array.Length; i++)
+        if(array[i] > max) max = array[i];
+        else
+            if(array[i] < min) min = array[i];
+    double result = max - min;
+    return result;
+}
+
+
+//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.Write("Задайте размер массива ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateRandomArray(size, 100, 999);
+ShowArray(array);
+Console.WriteLine("Чётных чисел в массиве " + EvenNumbers(array));
+
+
+// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Write("Задайте размер массива ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте минимальное значение ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте максимальное значение ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateRandomArray(size, min, max);
+ShowArray(array);
+Console.WriteLine("Сумма нечетных позиций " + SumOddPositions(array));
+
+//Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.Write("Задайте размер массива ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] array = CreateArray(size);
+Console.WriteLine("Разница максимального и минимального значения " + DifferenceMinMax(array));
--- Семинары/Seminar/Program.cs
+++ Семинары/Seminar/Program.cs
@@ -88,3 +88,108 @@ Console.Write("Введите второе число ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(Step(n, m));
 */
+
+
+//Создание массива случайным образом
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] newArray = new int[size];
+    for(int i = 0; i < size; i++)
+        newArray[i] = new Random().Next(min, max + 1);
+    return newArray;
+}
+
+// Создание массива вручную
+int[] CreateArray(int size)
+{
+    int[] newArray = new int[size];
+    Console.WriteLine("Creating array here: ");
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i + 1} element: ");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+    return newArray;
+}
+
+// Печать массива
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+// Сумма положительных элементов в массиве
+int FindPositiveSum(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+            sum += array[i];
+    }
+    return sum;
+}
+
+/*
+Console.Write("Input size of array ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of elements: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of elements: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array1 = CreateRandomArray(size, min, max);
+ShowArray(array1);
+int sumOfPositive = FindPositiveSum(array1);
+Console.WriteLine("Sum of positive elements in current array is " + sumOfPositive);
+*/
+
+
+//Напишите программу замена элементов массива:
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+int[] InversionArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        array[i] = - array[i];
+    return array;    
+}
+/*
+Console.Write("Input size of array ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of elements: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of elements: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array1 = CreateRandomArray(size, min, max);
+ShowArray(array1);
+int[] array2 = InversionArray(array1);
+ShowArray(array2);
+*/
+
+// Задайте одномерный массив из 20 случайных чисел. Найдите количество 
+// элементов массива, значения которых лежат в отрезке [10,99].
+int Interval(int[] array, int min, int max)
+{
+    int result = 0;
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] >= min && array[i] <= max) result++;
+    return result;
+}
+
+/*
+Console.Write("Input min : ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max : ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array1 = CreateRandomArray(20, -100, 100);
+ShowArray(array1);
+int result = Interval(array1, min, max);
+Console.WriteLine(result);
+*/
