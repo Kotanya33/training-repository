0659a07d5f0a88bcb419b4e625aa6080a84a822c
Tatyana <052025199@mail.ru>
2022-07-13 01:39:25 +0300

    homework 6

--- Семинары/HomeWork_6/Program.cs
+++ Семинары/HomeWork_6/Program.cs
@@ -12,20 +12,26 @@ int HowManyPositive(int m)
     Console.WriteLine();
     return result;
 }
-
+/*
 Console.WriteLine("Сколько чисел хотите ввести?");
 int m = Convert.ToInt32(Console.ReadLine());
 int positive = HowManyPositive(m);
 Console.WriteLine($"Положительных чисел {positive}");
-
+*/
 
 // Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
 // y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 void IintersectionOf2Lines(double b1, double k1, double b2, double k2)
 {
-    double x = (b2 - b1)/(k1-k2);
-    double y = k1 * x + b1;
-    Console.WriteLine($"Точка пересечения ({x};{y})");
+    if (k1 == k2 && b1 != b2) Console.WriteLine("Прямые не пересекаются");
+    else
+        if (k1 == k2 && b1 == b2) Console.WriteLine("Прямые совпадают");
+        else
+        {
+            double x = (b2 - b1)/(k1-k2);
+            double y = k1 * x + b1;
+            Console.WriteLine($"Точка пересечения ({x};{y})");
+        }
 }
 
 Console.Write("Введите b1 " );
