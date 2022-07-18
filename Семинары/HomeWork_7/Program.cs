872daf017f98e0d452e56d8fcc0cab416f757090
Tatyana <052025199@mail.ru>
2022-07-18 22:11:05 +0300

    HomeWork 7

---
+++ Семинары/HomeWork_7/Program.cs
@@ -0,0 +1,97 @@
+﻿//! Задайте двумерный массив разммером m*n, заполненный случайными вещественными числами.
+
+double[,] CreateRandom2dDoubleArray(int rows, int columns, int minValue, int maxValue)
+{
+    double[,] newarray = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newarray[i, j] = Math.Round(new Random().NextDouble() + new Random().Next(minValue, maxValue), 2);
+    return newarray;
+}
+void Show2dDoubleArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+double[,] myArray = CreateRandom2dDoubleArray(m,n,min,max);
+Show2dDoubleArray(myArray);
+
+//! Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//! и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+void ElementValue(double[,] array, int i, int j)
+{
+    if (i < array.GetLength(0) && j < array.GetLength(1))
+        Console.WriteLine(array[i,j]);
+    else
+        Console.WriteLine("Такого элемента нет");
+}
+
+Console.Write("Input number of rows: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int y = Convert.ToInt32(Console.ReadLine());
+ElementValue(myArray, x, y);
+
+//! Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] CreateRandom2DArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] newarray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newarray[i, j] = new Random().Next(minValue, maxValue + 1);
+    return newarray;
+}
+void Show2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+void ArithmeticalMeanOfColumn(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double mean = 0;
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+            sum = (sum + array[i,j]);
+        mean = Math.Round(sum / array.GetLength(0),2);
+        Console.Write(mean + "; ");
+    }
+}
+
+Console.Write("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2DArray(m,n,min,max);
+Show2DArray(myArray);
+Console.WriteLine();
+ArithmeticalMeanOfColumn(myArray);
--- Семинары/Seminar/Program.cs
+++ Семинары/Seminar/Program.cs
@@ -1,6 +1,6 @@
-﻿//Напишите программу, которая принимает на вход координаты точки (X и Y), 
-//причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
-/*
+﻿//!Напишите программу, которая принимает на вход координаты точки (X и Y), 
+//!причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
 int Koord(int x, int y)
 {
     int quad = 0;
@@ -13,17 +13,18 @@ int Koord(int x, int y)
     }
     return quad;
 }
-Console.Write("Введите x = ");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите y = ");
-int y = Convert.ToInt32(Console.ReadLine());
 
-int quad = Koord(x, y);
-Console.WriteLine(quad);
-*/
+// Console.Write("Введите x = ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите y = ");
+// int y = Convert.ToInt32(Console.ReadLine());
 
-//Напишите программу, которая принимает на вход число (N) и выдаёт квадраты чисел от 1 до N.
-/*
+// int quad = Koord(x, y);
+// Console.WriteLine(quad);
+
+
+//! Напишите программу, которая принимает на вход число (N) и выдаёт квадраты чисел от 1 до N.
+
 void Quad(int n)
 {
     int count = 1;
@@ -34,13 +35,14 @@ void Quad(int n)
         Console.WriteLine(proisv);
     }
 }
-Console.Write("Введите n ");
-int n = Convert.ToInt32(Console.ReadLine());
-Quad(n);
-*/
 
-//Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
-/*
+// Console.Write("Введите n ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Quad(n);
+
+
+//!Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
 int Kolich(int num)
 {
     int result = 1;
@@ -52,14 +54,14 @@ int Kolich(int num)
     }
     return result;
 }
-Console.Write("Введите число ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(Kolich(n));
-*/
+
+// Console.Write("Введите число ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Kolich(n));
 
 
-// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
-/*
+//! Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
 int Proisv(int N)
 {
     int result = 1;
@@ -67,14 +69,14 @@ int Proisv(int N)
         result = result * i;
     return result;
 }
-Console.Write("Введите число ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(Proisv(n));
-*/
+
+// Console.Write("Введите число ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Proisv(n));
 
 
-// Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-/*
+//! Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
 int Step(int a, int b)
 {
     int result = 1;
@@ -82,29 +84,32 @@ int Step(int a, int b)
         result = result * a;
     return result;
 }
-Console.Write("Введите первое число ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(Step(n, m));
-*/
+
+// Console.Write("Введите первое число ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Step(n, m));
 
 
-//Создание массива случайным образом
+
+//!Создание массива случайным образом
+
 int[] CreateRandomArray(int size, int min, int max)
 {
     int[] newArray = new int[size];
-    for(int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
         newArray[i] = new Random().Next(min, max + 1);
     return newArray;
 }
 
-// Создание массива вручную
+//! Создание массива вручную
+
 int[] CreateArray(int size)
 {
     int[] newArray = new int[size];
     Console.WriteLine("Creating array here: ");
-    for(int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
         Console.Write($"Input {i + 1} element: ");
         newArray[i] = Convert.ToInt32(Console.ReadLine());
@@ -113,92 +118,92 @@ int[] CreateArray(int size)
     return newArray;
 }
 
-// Печать массива
+//! Печать массива
+
 void ShowArray(int[] array)
 {
-    for(int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         Console.Write(array[i] + " ");
     }
     Console.WriteLine();
 }
 
-// Сумма положительных элементов в массиве
+//! Сумма положительных элементов в массиве
 int FindPositiveSum(int[] array)
 {
     int sum = 0;
-    for(int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        if(array[i] > 0)
+        if (array[i] > 0)
             sum += array[i];
     }
     return sum;
 }
 
-/*
-Console.Write("Input size of array ");
-int size = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input min possible value of elements: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input max possible value of elements: ");
-int max = Convert.ToInt32(Console.ReadLine());
 
-int[] array1 = CreateRandomArray(size, min, max);
-ShowArray(array1);
-int sumOfPositive = FindPositiveSum(array1);
-Console.WriteLine("Sum of positive elements in current array is " + sumOfPositive);
-*/
+// Console.Write("Input size of array ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input min possible value of elements: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input max possible value of elements: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[] array1 = CreateRandomArray(size, min, max);
+// ShowArray(array1);
+// int sumOfPositive = FindPositiveSum(array1);
+// Console.WriteLine("Sum of positive elements in current array is " + sumOfPositive);
 
 
-//Напишите программу замена элементов массива:
-// положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+//!Напишите программу замена элементов массива:
+//! положительные элементы замените на соответствующие отрицательные, и наоборот.
 int[] InversionArray(int[] array)
 {
-    for(int i = 0; i < array.Length; i++)
-        array[i] = - array[i];
-    return array;    
+    for (int i = 0; i < array.Length; i++)
+        array[i] = -array[i];
+    return array;
 }
-/*
-Console.Write("Input size of array ");
-int size = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input min possible value of elements: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input max possible value of elements: ");
-int max = Convert.ToInt32(Console.ReadLine());
 
-int[] array1 = CreateRandomArray(size, min, max);
-ShowArray(array1);
-int[] array2 = InversionArray(array1);
-ShowArray(array2);
-*/
+// Console.Write("Input size of array ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input min possible value of elements: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input max possible value of elements: ");
+// int max = Convert.ToInt32(Console.ReadLine());
 
-// Задайте одномерный массив из 20 случайных чисел. Найдите количество 
-// элементов массива, значения которых лежат в отрезке [10,99].
+// int[] array1 = CreateRandomArray(size, min, max);
+// ShowArray(array1);
+// int[] array2 = InversionArray(array1);
+// ShowArray(array2);
+
+
+//! Задайте одномерный массив из 20 случайных чисел. Найдите количество 
+//! элементов массива, значения которых лежат в отрезке [10,99].
 int Interval(int[] array, int min, int max)
 {
     int result = 0;
-    for(int i = 0; i < array.Length; i++)
-        if(array[i] >= min && array[i] <= max) result++;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] >= min && array[i] <= max) result++;
     return result;
 }
 
-/*
-Console.Write("Input min : ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input max : ");
-int max = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input min : ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input max : ");
+// int max = Convert.ToInt32(Console.ReadLine());
 
-int[] array1 = CreateRandomArray(20, -100, 100);
-ShowArray(array1);
-int result = Interval(array1, min, max);
-Console.WriteLine(result);
-*/
+// int[] array1 = CreateRandomArray(20, -100, 100);
+// ShowArray(array1);
+// int result = Interval(array1, min, max);
+// Console.WriteLine(result);
 
-// Напишите программу, которая перевернёт одномерный массив 
-// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+//! Напишите программу, которая перевернёт одномерный массив 
+//! (последний элемент будет на первом месте, а первый - на последнем и т.д.)
 int[] ReverseArray(int[] array)
 {
-    for(int i = 0, j = array.Length - 1; i < j; i++, j--)
+    for (int i = 0, j = array.Length - 1; i < j; i++, j--)
     {
         int temp = array[i];
         array[i] = array[j];
@@ -206,17 +211,17 @@ int[] ReverseArray(int[] array)
     }
     return array;
 }
-/*
-int[] myArray = {3, 5, 1, 8, 2, 9, 4};
-myArray = ReverseArray(myArray);
-ShowArray(myArray);
-*/
 
-//Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// int[] myArray = {3, 5, 1, 8, 2, 9, 4};
+// myArray = ReverseArray(myArray);
+// ShowArray(myArray);
+
+
+//! Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 string ChangeDigitSystem(int num)
 {
     string resultNumber = string.Empty;
-    while(num > 0)
+    while (num > 0)
     {
         resultNumber = num % 2 + resultNumber;
         num /= 2; // num = num / 2;
@@ -225,8 +230,8 @@ string ChangeDigitSystem(int num)
 }
 // Console.WriteLine(ChangeDigitSystem(18));
 
-// Напишите программу, которая принимает на вход три числа и проверяет, 
-// может ли существовать треугольник с сторонами такой длины.
+//! Напишите программу, которая принимает на вход три числа и проверяет, 
+//! может ли существовать треугольник с сторонами такой длины.
 void Tr(int a, int b, int c)
 {
     if (a < b + c && b < a + c && c < a + b)
@@ -234,41 +239,246 @@ void Tr(int a, int b, int c)
     else
         Console.WriteLine("нет");
 }
-/*
-Console.Write("Введите a " );
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите b " );
-int b = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите c " );
-int c = Convert.ToInt32(Console.ReadLine());
-Tr(a, b, c);
-*/
+
+// Console.Write("Введите a " );
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите b " );
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите c " );
+// int c = Convert.ToInt32(Console.ReadLine());
+// Tr(a, b, c);
 
 bool Tr1(int a, int b, int c)
 {
-    if(a < b + c && b < a + c && c < a + b) return true;
+    if (a < b + c && b < a + c && c < a + b) return true;
     else return false;
 }
 // Console.WriteLine(Tr1(1,2,2));
 
 
-// Не используя рекурсию, выведите первые N чисел Фибоначчи. 
-// Первые два числа Фибоначчи: 0 и 1.
-int [] Fibonacci(int n)
+//! Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+//! Первые два числа Фибоначчи: 0 и 1.
+int[] Fibonacci(int n)
 {
-    
     int[] newArray = new int[n];
     Console.WriteLine("Creating array here: ");
     newArray[0] = 0;
     newArray[1] = 1;
-    for(int i = 2; i < n; i++)
+    for (int i = 2; i < n; i++)
     {
-        newArray[i] = newArray[i - 1] + newArray[ i - 2];
+        newArray[i] = newArray[i - 1] + newArray[i - 2];
     }
     return newArray;
 }
-/*
-Console.Write("Введите число ");
-int num = Convert.ToInt32(Console.ReadLine());
-ShowArray(Fibonacci(num));
-*/
+
+// Console.Write("Введите число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// ShowArray(Fibonacci(num));
+
+
+//! Задайте двумерный массив размером m×n, 
+//! заполненный случайными целыми числами.
+int[,] CreateRandom2DArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] newarray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newarray[i, j] = new Random().Next(minValue, maxValue + 1);
+    return newarray;
+}
+
+//! Печать двумерного массива
+void Show2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+// Console.Write("Input number of rows: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number of columns: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input min possible value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input max possible value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[,] myArray = CreateRandom2DArray(m,n,min,max);
+// Show2DArray(myArray);
+
+
+//! Задайте двумерный массив размера m на n, 
+//! каждый элемент в массиве находится по формуле: Aij = i+j. 
+//! Выведите полученный массив на экран.
+int[,] ArraySumElements(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            array[i, j] = i + j;
+    return array;
+}
+
+// Console.Write("Input number of rows: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number of columns: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,] newArray = ArraySumElements(m,n);
+// Show2DArray(newArray);
+
+
+//! Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные,
+//! и замените эти элементы на их квадраты.
+int[,] ArrayQuad(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            if (i % 2 == 0 && j % 2 == 0)
+                array[i, j] = array[i, j] * array[i, j];
+    return array;
+}
+
+// Console.Write("Input number of rows: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number of columns: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input min possible value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input max possible value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[,] myArray = CreateRandom2DArray(m,n,min,max);
+// Show2DArray(myArray);
+// Console.WriteLine();
+// ArrayQuad(myArray);
+// Show2DArray(myArray);
+
+
+//! Задайте двумерный массив. Найдите сумму элементов, находящихся на 
+//! главной диагонали (с индексами (0,0); (1;1) и т.д.
+int findDiagSum(int[,] array)
+{
+    int sum = 0;
+    if (array.GetLength(0) == array.GetLength(1))
+        for (int i = 0; i < array.GetLength(0); i++)
+            sum += array[i, i];
+
+    return sum;
+}
+
+// Console.Write("Input number of rows: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number of columns: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input min possible value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input max possible value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[,] myArray = CreateRandom2DArray(m,n,min,max);
+// Show2DArray(myArray);
+// Console.WriteLine("Sum of main diagonal is " + findDiagSum(myArray));
+
+//! ЗАДАЧА 1. Задайте двумерный массив. Напишите программу, 
+//! которая поменяет местами первую и последнюю строку массива.
+int[,] FirstLastRows(int[,] array)
+{
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+        int temp = array[0, j];
+        array[0,j] = array[array.GetLength(0) - 1, j];
+        array[array.GetLength(0) - 1, j] = temp;
+    }
+    return array;
+}
+// Console.Write("Input number of rows: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number of columns: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input min possible value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input max possible value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[,] myArray = CreateRandom2DArray(m,n,min,max);
+// Show2DArray(myArray);
+// Console.WriteLine();
+// myArray = FirstLastRows(myArray);
+// Show2DArray(myArray);
+
+//! ЗАДАЧА 2. Задайте двумерный массив. Напишите программу, 
+//! которая заменяет строки на столбцы. В случае, если это невозможно, 
+//! программа должна вывести сообщение для пользователя.
+int[,] Reverse2dArray(int[,] array)
+{
+    if(array.GetLength(0) != array.GetLength(1))
+    {
+        Console.WriteLine("Number of rows and columns is not the same!");
+        return array;
+    }
+
+    for(int i = 0; i < array.GetLength(0) - 1; i++)
+        for(int j = i + 1; j < array.GetLength(1); j++)
+        {
+            int temp = array[i,j];
+            array[i,j] = array[j,i];
+            array[j,i] = temp;
+        }
+    return array;
+}
+
+// Console.Write("Input number of rows: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number of columns: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input min possible value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input max possible value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[,] myArray = CreateRandom2DArray(m,n,min,max);
+// Show2DArray(myArray);
+// Console.WriteLine();
+// myArray = Reverse2dArray(myArray);
+// Show2DArray(myArray);
+
+//! ЗАДАЧА 3. Из двумерного массива целых чисел удалить строку и столбец, 
+//! на пересечении которых расположен наименьший элемент.
+int[,] Cut2dArray(int[,] array)
+{
+    int iMin = 0, jMin = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 1; j < array.GetLength(1); j++)
+        {
+            if(array[i,j] < array[iMin, jMin])
+            {
+                iMin = i;
+                jMin = j;
+            }
+        }
+    for(int j = 0; j < array.GetLength(1); j++)
+        array[iMin,j] = 0;
+    for(int i = 0; i < array.GetLength(0); i++)
+        array[i,jMin] = 0;
+    return array;
+}
+
+// Console.Write("Input number of rows: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number of columns: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input min possible value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input max possible value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[,] myArray = CreateRandom2DArray(m,n,min,max);
+// Show2DArray(myArray);
+// Console.WriteLine();
+// myArray = Cut2dArray(myArray);
+// Show2DArray(myArray);
